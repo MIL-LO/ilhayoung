9c443b7d4d42a8696732ac6056a151021b816022
악덕 <rudejrqkr97@gmail.com>
2025-06-28 10:15:22 +0900

    init

---
+++ src/main/kotlin/com/millo/ilhayoung/IlhayoungApplication.kt
@@ -0,0 +1,11 @@
+package com.millo.ilhayoung
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IlhayoungApplication
+
+fun main(args: Array<String>) {
+    runApplication<IlhayoungApplication>(*args)
+}
---
+++ src/test/kotlin/com/millo/ilhayoung/IlhayoungApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.millo.ilhayoung
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IlhayoungApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
