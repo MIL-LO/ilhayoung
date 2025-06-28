df10e76106ea6ea39b452d4186f542983bd9d388
악덕 <rudejrqkr97@gmail.com>
2025-06-28 11:01:14 +0900

    refactor: 클래스 이름 변경
    - 'IlhayoungApplication'에서 'Application'으로 클래스 이름 변경
    - 'IlhayoungApplicationTests'에서 'ApplicationTests'으로 클래스 이름 변경
    - 코드의 일관성을 위해 변경

--- src/main/kotlin/com/millo/ilhayoung/Application.kt
+++ src/main/kotlin/com/millo/ilhayoung/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class IlhayoungApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<IlhayoungApplication>(*args)
+    runApplication<Application>(*args)
 }
