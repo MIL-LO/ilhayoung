94d7744b974fc96ef67ed3427ceb7802a850bc22
악덕 <rudejrqkr97@gmail.com>
2025-06-28 11:12:46 +0900

    fix: build.gradle.kts 파일에서 불필요한 의존성 제거 및 추가
    - spring-boot-starter-data-jpa 의존성을 제거함
    - spring-boot-starter-web 의존성을 추가함

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // Databases
@@ -32,6 +31,7 @@ dependencies {
 
     // Swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     // Tests
     testImplementation("org.springframework.boot:spring-boot-starter-test")
