2571bada5dc94491a2ca8c2231d27c60415ede80
악덕 <rudejrqkr97@gmail.com>
2025-07-09 14:25:18 +0900

    feat: Application 클래스에 ObjectMapper 빈 등록
    - ObjectMapper를 빈으로 등록하여 전역에서 사용 가능하게 함
    - Kotlin 모듈, JavaTime 모듈 등록 및 날짜를 타임스탬프로 변환하는 기능 비활성화

--- src/main/kotlin/com/millo/ilhayoung/Application.kt
+++ src/main/kotlin/com/millo/ilhayoung/Application.kt
@@ -1,10 +1,23 @@
 package com.millo.ilhayoung
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class Application
+class Application {
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper()
+            .registerKotlinModule()
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
